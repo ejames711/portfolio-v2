d661dc5e83c2ab49537238267fdcf29c7b21cd56
TrikKH <ejames711@gmail.com>
2023-01-31 01:34:44 -0600

    finish content

---
+++ app.js
@@ -0,0 +1,8 @@
+const navbar = document.getElementsByClassName('nav-fixed');
+window.onscroll = () => {
+    if (window.scrollY > 50) {
+        navbar[0].classList.add('nav-active');
+    } else {
+        navbar[0].classList.remove('nav-active');
+    }
+};
